b1d2d2f669ff50298f4e4b175dc558cbaddacb6b
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2018-11-22 18:18:53 +0900

    Refactor

--- lib/ulid/rails/formatter.rb
+++ lib/ulid/rails/formatter.rb
@@ -4,19 +4,11 @@ module ULID
   module Rails
     module Formatter
       def self.format(v)
-        if v.length == 32
-          Base32::Crockford.encode(v.hex)
-        else
-          v
-        end
+        v.length == 32 ? Base32::Crockford.encode(v.hex) : v
       end
 
       def self.unformat(v)
         Base32::Crockford.decode(v).to_s(16).rjust(32, "0")
-      end
-
-      def self.regexp
-        /\A[[:alnum:]]{25}\z/
       end
     end
   end
