40f44a8be69b4360276bbac59a538a0273081eac
Attila Večerek <avecerek@zendesk.com>
2022-03-23 18:32:52 +0100

    Fix ULID validator

--- lib/ulid/rails/validator.rb
+++ lib/ulid/rails/validator.rb
@@ -3,9 +3,14 @@ require "base32/crockford"
 module ULID
   module Rails
     module Validator
+      INVALID_CHARACTERS_REGEX = /[ilou]/i.freeze
+
       def self.is_valid?(v)
         return true if v.nil?
-        v.length == 26 && Base32::Crockford.valid?(v)
+        return false unless v.length == 26
+        return false if INVALID_CHARACTERS_REGEX.match?(v)
+        
+        Base32::Crockford.valid?(v)
       end
     end
   end
