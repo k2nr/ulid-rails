a371fab56dc38250dceb1d2d06eae53bc34673ef
Benjamin Quorning <benjamin@quorning.net>
2023-07-12 12:54:53 +0200

    Add debug gem, add comments

--- lib/ulid/rails/formatter.rb
+++ lib/ulid/rails/formatter.rb
@@ -4,12 +4,16 @@ module ULID
   module Rails
     module Formatter
       def self.format(v)
+        # v is 32 lowercase hex
+        # sanitized is 37 integers
         sanitized = v.delete("-").hex
         Base32::Crockford.encode(sanitized).rjust(26, "0")
       end
 
       def self.unformat(v)
         decoded_i = Base32::Crockford.decode(v.to_s).to_i
+        # v is 26 digits/uppercase letters
+        # decoded_i is 37 integers
         decoded_i.to_s(16).rjust(32, "0")
       end
     end
--- ulid-rails.gemspec
+++ ulid-rails.gemspec
@@ -36,6 +36,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "activemodel", ">= 5.0"
   spec.add_dependency "activerecord", ">= 5.0"
   spec.add_development_dependency "bundler"
+  spec.add_development_dependency "debug"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "minitest", "~> 5.0"
   spec.add_development_dependency "rubocop-minitest"
