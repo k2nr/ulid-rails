396425f423c3e31b2ee1ce27b83da2628a25df5f
Benjamin Quorning <benjamin@quorning.net>
2023-08-23 14:24:21 +0200

    Add many more tests
    I don't really have confidence in the singular `test_eager_loading` test, so
    adding tests that checks use of `includes`, `joins`, `left_outer_joins` and
    `preload` too.
    
    We also added tests to ensure we can read the format we have been writing to
    the database so far, and that we write to the database in the same format as
    usual.

--- test/ulid/rails_test.rb
+++ test/ulid/rails_test.rb
@@ -5,6 +5,35 @@ class ULID::RailsTest < Minitest::Test
     model_classes.each(&:delete_all)
   end
 
+  def test_stores_the_ulid_as_binary_value_in_the_database
+    user = User.create!(id: "01ARZ3NDEKTSV4RRFFQ69G5FAV")
+
+    raw_id = user.read_attribute_before_type_cast(:id).hex
+    expected_raw_id = {
+      "mysql2" => "01563e3ab5d3d6764c61efb99302bd5b",
+      "sqlite3" => "01563e3ab5d3d6764c61efb99302bd5b",
+      "postgresql" => "\x01V>:\xB5\xD3\xD6vLa\xEF\xB9\x93\x02\xBD[".b
+    }.fetch(database_adapter)
+
+    assert_equal expected_raw_id, raw_id
+
+    binary_stored_id = User.last.read_attribute_before_type_cast(:id)
+    expected_binary_stored_id = {
+      "mysql2" => "\x01V>:\xB5\xD3\xD6vLa\xEF\xB9\x93\x02\xBD[".b,
+      "sqlite3" => "01563e3ab5d3d6764c61efb99302bd5b".b,
+      "postgresql" => "\\x01563e3ab5d3d6764c61efb99302bd5b".b
+    }.fetch(database_adapter)
+
+    assert_equal expected_binary_stored_id, binary_stored_id
+  end
+
+  def test_deserializes_the_value_back_to_an_ulid
+    user = User.create!(id: "01ARZ3NDEKTSV4RRFFQ69G5FAV")
+    user.reload
+
+    assert_equal "01ARZ3NDEKTSV4RRFFQ69G5FAV", user.id
+  end
+
   def test_auto_generate_primary_key
     user = User.create!
 
@@ -68,7 +97,7 @@ class ULID::RailsTest < Minitest::Test
     assert_equal user.articles[0], article
   end
 
-  def test_eager_loading
+  def test_loading_associations_with_eager_load
     user = User.create!
     user.books.create!
 
@@ -77,13 +106,65 @@ class ULID::RailsTest < Minitest::Test
     assert_equal [user], result
   end
 
-  def test_eager_loading_limit
+  def test_loading_associations_with_eager_load_with_limit
     user = User.create!
     user.books.create!
 
     result = User.eager_load(:books).limit(1).to_a
 
     assert_equal [user], result
+  end
+
+  def test_loading_associations_with_includes
+    user = User.create!
+    user.books.create!
+
+    result = User.includes(:books).to_a
+
+    assert_equal [user], result
+  end
+
+  def test_loading_associations_with_joins
+    user = User.create!
+    user.books.create!
+
+    result = User.joins(:books).to_a
+
+    assert_equal [user], result
+  end
+
+  def test_loading_associations_with_left_outer_joins
+    user = User.create!
+    user.books.create!
+
+    result = User.left_outer_joins(:books).to_a
+
+    assert_equal [user], result
+  end
+
+  def test_loading_associations_with_preload
+    user = User.create!
+    user.books.create!
+
+    result = User.preload(:books).to_a
+
+    assert_equal [user], result
+  end
+
+  def test_type_casting
+    user = User.create!(id: "01ARZ3NDEKTSV4RRFFQ69G5FAV")
+
+    user.reload
+
+    raw_id = user.read_attribute_before_type_cast(:id)
+    expected_raw_id = {
+      "mysql2" => "\x01V>:\xB5\xD3\xD6vLa\xEF\xB9\x93\x02\xBD[".b,
+      "sqlite3" => "01563e3ab5d3d6764c61efb99302bd5b".b,
+      "postgresql" => "\\x01563e3ab5d3d6764c61efb99302bd5b".b
+    }.fetch(database_adapter)
+
+    assert_equal expected_raw_id, raw_id
+    assert_equal "01ARZ3NDEKTSV4RRFFQ69G5FAV", user.id
   end
 
   private
